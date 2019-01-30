89bfd37aa5857e6d521f16112fe6928ff69eeff2
kapaidashu <kasia0001@126.com>
2019-01-30 16:19:11 +0800

    add stock

--- QUANTAXIS/ceshi.py
+++ QUANTAXIS/ceshi.py
@@ -61,11 +61,24 @@ def task(futureName, starttime):
 
 if __name__ == '__main__':
    today = datetime.datetime.now().strftime('%Y-%m-%d')
-   task('J1905', date_add(today,-7))
-   task('RB1905', date_add(today,-7))
-   task('HC1905', date_add(today,-7))
-   task('I1905', date_add(today,-7))
-   task('RU1905', date_add(today,-7))
-   task('M1905', date_add(today,-7))
-   task('AP1905', date_add(today,-7))
-   task('FU1905', date_add(today,-60))
+   task('J1905', date_add(today,-7))   #焦炭
+   task('RB1905', date_add(today,-7))  #螺纹钢
+   task('HC1905', date_add(today,-7))  #热卷
+   task('I1905', date_add(today,-7))   #铁矿石
+   task('RU1905', date_add(today,-7))  #橡胶
+   task('M1905', date_add(today,-7))   #豆粕
+   task('AP1905', date_add(today,-7))  #苹果
+   task('TA1905', date_add(today, -7)) #PTA
+   task('FU1905', date_add(today,-7))  #燃油
+   task('EG1906', date_add(today, -7)) #乙二醇
+   task('BU1906', date_add(today, -7))  # 沥青
+   task('FG1905', date_add(today, -7))  # 玻璃
+   task('RM1905', date_add(today, -7))  # 菜粕
+   task('ZC1905', date_add(today, -7))  # 郑煤
+   task('P1905', date_add(today, -7))  # 棕榈
+   task('SM1905', date_add(today, -7))  # 硅锰
+   task('SF1905', date_add(today, -7))  # 硅铁
+   task('MA1905', date_add(today, -7))  # 郑醇
+   task('JM1905', date_add(today, -7))  # 焦煤
+   task('SP1906', date_add(today, -7))  # 纸浆
+   task('CF1905', date_add(today, -7))  # 郑棉
